07a22738b5e2c01de688df8381920d109b6b2ac1
Dario Carlino <baires82@gmail.com>
2020-09-03 15:26:41 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,25 @@
-# Your code goes here!
+require "pry"
+class Anagram
+  attr_accessor :word
+  @word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(anagram)
+    result = []
+    a = @word.split("") #=> splits the initialized "word"
+    a = a.sort
+    anagram.each do |ana| #=> ana spits out each value in [anagram]
+      b = ana.split("") #=> splits ana
+      b = b.sort
+      if a == b
+        result << ana
+      end
+      #binding.pry
+    end
+    result
+  end
+
+end
