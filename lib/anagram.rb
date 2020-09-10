f0b039145c9cdd8869225d3c9411e26b008411b5
tjennings343 <tjennings343@gmail.com>
2020-09-10 19:49:56 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
-# Your code goes here!
+require 'pry'
+class Anagram
+    
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(input)
+      input.select {|x| x.split("").sort == @word.split("").sort }  
+    end
+
+end
