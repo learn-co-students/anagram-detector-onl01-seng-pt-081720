6e4ce11bd8302ee0141f462aaf93249d94d15cef
Hector_Jimenez <hjmontemayor@gmail.com>
2020-09-11 19:24:59 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+    attr_accessor :anagram, :phrase
+
+    def initialize(anagram)
+        @anagram = anagram
+    end
+
+    def match=(phrase)
+        @phrase = phrase
+    end
+
+    def match(phrase)
+        anagram_answer = []
+        phrase.each do |x| 
+            if ((x.chars.sort.join) == @anagram.chars.sort.join)
+                anagram_answer << x
+            end
+        end
+        anagram_answer
+        
+    end
+end
