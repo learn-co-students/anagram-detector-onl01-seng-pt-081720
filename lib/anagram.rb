d47773f1aca405123edf84dbdb402783d501ea71
Tim Flood <tflood9@gmail.com>
2020-09-11 20:37:36 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+require 'pry'
+class Anagram
+  def initialize(word)
+    @word = word
+  end
+
+  def match(list)
+    is_anagram = []
+    letters = @word.split("")
+    list.each do |item|
+      item_letters = item.split("")
+      if item_letters.sort == letters.sort
+        is_anagram << item
+      end
+    end
+      is_anagram
+  end
+end
