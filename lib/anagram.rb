033b20e2aab20506214d00cbb73fb380fe087f8b
Haidy  Farahat <haidyfarahat@gmail.com>
2020-09-13 17:48:00 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+    @word = word.split("").sort
+  end
+
+  def match(words)
+    x = []
+    words.each do  |element|
+        splitted = element.split("").sort
+        if splitted == @word
+          x << element
+        end
+    end
+    return x
+  end
+end
